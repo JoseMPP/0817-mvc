31519a1e23387321afec43844efba4a62cc2b18a
user <josmapizarro.99@gmail.com>
2019-08-17 08:55:45 -0400

    mvc

---
+++ MVCstudent/MVCstudent/Models/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace MVCstudent.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext() : base("DefaultConnection") {
+
+        }
+    }
+}
---
+++ MVCstudent/MVCstudent/Models/student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Web;
+
+namespace MVCstudent.Models
+{
+    public class student
+    {
+        [Key]
+        public int studentID { get; set; }
+        [Required]
+        [Display(Description ="LastName")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Description ="First Name")]
+        public string FirstName { get; set; }
+    }
+}
